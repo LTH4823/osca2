0331a4bc2bd649dfe2e2ab7389e46350835d774a
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2023-09-12 08:14:06 +0900

    Initial commit

---
+++ src/main/java/org/taerock/osca2/Osca2Application.java
@@ -0,0 +1,13 @@
+package org.taerock.osca2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Osca2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Osca2Application.class, args);
+    }
+
+}
